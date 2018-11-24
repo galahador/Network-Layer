24ca1659adc44154957f59a460e26f3dc5fd02fd
CodeTribe <codetribe_macos@CodeTribes-MacBook-Pro.local>
2018-11-24 19:33:44 +0100

    Add NetworkLoger

---
+++ NetworkLayer/Networking/Service/NetworkLogger.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkLogger.swift
+//  NetworkLayer
+//
+//  Created by CodeTribe on 11/24/18.
+//  Copyright © 2018 Petar Lemajic. All rights reserved.
+//
+
+import Foundation
+
+class NetworkLogger {
+
+static func log(response: URLResponse) {}
+    
+}
